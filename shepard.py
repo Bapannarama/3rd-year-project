47ec3918f147555eeea29cacc405e4cfedcbb80a
Bapannarama <bbhamid0@gmail.com>
2015-02-28 18:58:18 +0000

    finished testing functions

--- shepard.py
+++ shepard.py
@@ -1,1 +1,70 @@
 __author__ = 'bapanna'
+
+import numpy as np
+import math
+
+
+def denominator(w):
+	return np.sum(w)
+
+
+def numerator(w):
+	x = 0
+	y = 0
+
+	for i in range(len(w)):
+		for j in range(len(w[0])):
+			x += i * w[i][j]
+			y += j * w[i][j]
+
+	return [x,y]
+
+
+def euclidean_distance(r, s):
+	r = np.asarray(r)
+	s = np.asarray(s)
+
+	return np.linalg.norm(r - s)
+
+
+def calc_weights(database, rssi_vec, p):
+	w = [[0 for i in range(len(database))] for i in range(len(database[0]))]
+
+	for i in range(len(database)):
+		for j in range(len(database[i])):
+			# prevents division by zero when testing with zeroed vector from tests.py
+			if database[i][j][0] == 0:
+				w[i][j] = 0
+				continue
+
+			data_vector = database[i][j]
+
+			w[i][j] = euclidean_distance(data_vector, rssi_vec) ** -p
+
+	return w
+
+
+# checks if input vector corresponds to RSSIs of a known point
+def check_input_vector(database, vector):
+	for i, col in enumerate(database):
+		for j, strengths in enumerate(col):
+			if strengths == vector:
+				return [i,j]
+
+	return False
+
+
+# rssi will be a list
+def shepard_interpolation(rssi_data, rssi_vector, power):
+	location = check_input_vector(rssi_data, rssi_vector)
+
+	if location:
+		return location
+
+	weights = calc_weights(rssi_data, rssi_vector, power)
+	num = numerator(weights)
+	den = denominator(weights)
+
+	coordinates = num / den
+
+	return coordinates
--- tests.py
+++ tests.py
@@ -3,7 +3,35 @@ __author__ = 'bapanna'
 import data_fetch as df
 import data_repr as dr
 import numpy as np
+import shepard as sp
+
+def generate_errors_matrix(rssi, function):
+	"""This function has 'function' representing the interpolation scheme as a
+	parameter. This allows flexibility in which method is used to find position
+	estimates. The rssi parameter is expected to be averaged."""
+
+	errors = [[0 for x in range(len(rssi))] for x in range(len(rssi[0]))]
+
+	for i in range(len(rssi)):
+		for j in range(len(rssi[i])):
+			# test_vector contains the rssi values of the coordinate to be tested
+			test_vector = rssi[i][j]
+
+			# test_database is the database which the test_vector will be tested against
+			rssi[i][j] = [0, 0, 0, 0]
+
+			test_coordinate = np.asarray((i,j))
+			calc_coordinate = function(rssi, test_vector, 2)
+
+			calc_coordinate = np.asarray(calc_coordinate)
+			errors[i][j] = np.linalg.norm(test_coordinate - calc_coordinate) * 50
+
+			rssi[i][j] = test_vector
+
+	return errors
 
 dataset = 'elevated'
 s, t = df.fetch_data(dataset)
-dr.diagonal_rssi(dataset, s)
+s = df.rssi_average(s)
+func = sp.shepard_interpolation
+error_matrix = generate_errors_matrix(s, func)
